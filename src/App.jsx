e30fd594084687eff57a0e7540de812e3069af79
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-21 23:45:32 -0500

    setting the initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <header className="py-5">
+        <h1>Beverage Finder</h1>
+      </header>
+      <Container className="mt-5">1</Container>
+    </>
+  );
+}
+
+export default App;
