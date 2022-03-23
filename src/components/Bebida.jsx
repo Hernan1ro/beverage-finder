fd3d825c14a13691e74951f9ccf503752dcbb90a
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-22 23:12:54 -0500

    adding card component to bootstrap

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Col, Card, Button } from "react-bootstrap";
 
 const Bebida = ({ bebida }) => {
-  return <div>{bebida.strDrink}</div>;
+  return (
+    <Col md={6} lg={3}>
+      <Card className={"mb-4"}>
+        <Card.Img
+          variant="top"
+          src={bebida.strDrinkThumb}
+          alt={bebida.strDrink}
+        />
+        <Card.Body>
+          <Card.Title>{bebida.strDrink}</Card.Title>
+          <Button className="w-100 text-uppercase mt-2" variant="warning">
+            See recipe
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
 };
 
 export default Bebida;
--- src/components/ListadoBebidas.jsx
+++ src/components/ListadoBebidas.jsx
@@ -5,7 +5,7 @@ import Bebida from "./Bebida";
 const ListadoBebidas = () => {
   const { bebidas } = useBebidas();
   return (
-    <Row>
+    <Row className="mt-5">
       {bebidas.map((bebida) => (
         <Bebida bebida={bebida} key={bebida.idDrink} />
       ))}
