9716d7a6d25d3d7f98ea711aafb7d2108d1a00f3
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-23 01:06:53 -0500

    showing recipes details in the modal

--- src/components/ModalBebida.jsx
+++ src/components/ModalBebida.jsx
@@ -2,11 +2,36 @@ import { Modal, Image } from "react-bootstrap";
 import useBebidas from "../hooks/useBebidas";
 
 const ModalBebida = () => {
-  const { modal, handleModalClick } = useBebidas();
+  const { modal, handleModalClick, receta } = useBebidas();
+
+  const MostrarIngredientes = () => {
+    let ingredientes = [];
+    for (let i = 1; i < 16; i++) {
+      if (receta[`strIngredient${i}`]) {
+        ingredientes.push(
+          <li>
+            {receta[`strIngredient${i}`]} ${receta[`strMeasure${i}`]}
+          </li>
+        );
+      }
+    }
+    return ingredientes;
+  };
 
   return (
     <Modal show={modal} onHide={handleModalClick}>
-      <Modal.Body>Cuerpo del modal</Modal.Body>
+      <Image src={receta.strDrinkThumb} alt={receta.strDrinkThumb} />
+      <Modal.Header>
+        <Modal.Title>{receta.strDrink}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="p-3">
+          <h2>Instructions</h2>
+          {receta.strInstructions}
+          <h2>Ingredients and quantities</h2>
+          {MostrarIngredientes()}
+        </div>
+      </Modal.Body>
     </Modal>
   );
 };
