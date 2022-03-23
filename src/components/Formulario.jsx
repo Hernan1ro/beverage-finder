bfcfb52ba3d55b7a52722b1aa8ccf9e30e8ac8ed
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-22 21:42:40 -0500

    rendering every category

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,8 @@
 import { Button, Form, Row, Col } from "react-bootstrap";
+import useCategorias from "../hooks/useCategorias";
 
 const Formulario = () => {
+  const { categorias } = useCategorias();
   return (
     <Form>
       <Row>
@@ -20,8 +22,23 @@ const Formulario = () => {
             <Form.Label htmlFor="category">Beverage Category</Form.Label>
             <Form.Select name="category" id="category">
               <option value="">- Select a category</option>
+              {categorias.map((categoria) => (
+                <option
+                  value={categoria.strCategory}
+                  key={categoria.strCategory}
+                >
+                  {categoria.strCategory}
+                </option>
+              ))}
             </Form.Select>
           </Form.Group>
+        </Col>
+      </Row>
+      <Row className="justify-content-end">
+        <Col md={3}>
+          <Button variant="danger" className="text-uppercase w-100">
+            Find Beverage
+          </Button>
         </Col>
       </Row>
     </Form>
